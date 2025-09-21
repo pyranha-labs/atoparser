05d7c3c020dedaf4e41d7591fb229562ecb37fbb
David <dfrtzdev@gmail.com>
2025-09-21 13:37:11 -0600

    chg: Increase test coverage to 95%+. (#50)

--- atoparser/reader.py
+++ atoparser/reader.py
@@ -45,11 +45,13 @@ def parse_args() -> argparse.Namespace:
     )
     parser.add_argument(
         "--tstats",
+        dest="show_tstats",
         action="store_true",
         help="Include TStats/PStats in output. Very verbose.",
     )
     parser.add_argument(
         "--cstats",
+        dest="show_cstats",
         action="store_true",
         help="Include CGroup/CStats in output. Only available with Atop 2.11+ logs. Verbose.",
     )
@@ -57,31 +59,42 @@ def parse_args() -> argparse.Namespace:
     return args
 
 
-def main() -> None:
-    """Primary function to load Atop data."""
-    args = parse_args()
+def parse_file(  # pylint: disable=too-many-locals
+    file: str,
+    parseables: list[str] | None = None,
+    pretty_print: bool = True,
+    show_tstats: bool = False,
+    show_cstats: bool = False,
+) -> None:
+    """Parse a single file.
 
-    for file in args.files:
-        samples = []
-        opener = open if ".gz" not in file else gzip.open
-        with opener(file, "rb") as raw_file:
-            header = atoparser.get_header(raw_file)
-            if args.parseables and header.semantic_version not in PARSEABLE_MAP:
-                samples.append(
-                    {
-                        "error": f"Atop version {header.semantic_version} does not support parseables, only full raw output.",
-                        "file": file,
-                    }
-                )
-                continue
+    Args:
+        file: File path to process.
+        parseables: Atop "parseable" formats to display, instead of full structs.
+        pretty_print: Whether to pretty print the JSON output with indentation.
+        show_tstats: Whether to include TStats/PStats in output.
+        show_cstats: Wheter to include CGroup/CStats in output
+    """
+    samples = []
+    opener = open if ".gz" not in file else gzip.open
+    with opener(file, "rb") as raw_file:
+        header = atoparser.get_header(raw_file)
+        if parseables and header.semantic_version not in PARSEABLE_MAP:
+            samples.append(
+                {
+                    "error": f"Atop version {header.semantic_version} does not support parseables, only full raw output.",
+                    "file": file,
+                }
+            )
+        else:
             parsers = PARSEABLE_MAP.get(header.semantic_version, PARSEABLE_MAP["1.26"])
             for record, sstat, tstats, cgroups in atoparser.generate_statistics(
                 raw_file,
                 header,
                 raise_on_truncation=False,
             ):
-                if args.parseables:
-                    for parseable in args.parseables:
+                if parseables:
+                    for parseable in parseables:
                         for sample in parsers[parseable](header, record, sstat, tstats):
                             sample["parseable"] = parseable
                             samples.append(sample)
@@ -91,12 +104,26 @@ def main() -> None:
                         "record": atoparser.struct_to_dict(record),
                         "sstat": atoparser.struct_to_dict(sstat),
                     }
-                    if args.tstats:
+                    if show_tstats:
                         converted["tstat"] = [atoparser.struct_to_dict(stat) for stat in tstats]
-                    if args.cstats:
+                    if show_cstats:
                         converted["cgroup"] = [atoparser.struct_to_dict(stat) for stat in cgroups]
                     samples.append(converted)
-        print(json.dumps(samples, indent=2 if args.pretty_print else None))
+    print(json.dumps(samples, indent=2 if pretty_print else None))
+
+
+def main() -> None:
+    """Primary function to load Atop data."""
+    args = parse_args()
+
+    for file in args.files:
+        parse_file(
+            file,
+            args.parseables,
+            args.pretty_print,
+            args.show_tstats,
+            args.show_cstats,
+        )
 
 
 if __name__ == "__main__":
